cffcd3f390d7f3fc5a9ed46067e720e3e5f03174
President Carter <olabodebalogun80@gmail.com>
2024-01-20 11:00:56 +0100

    fixed

--- server/main/api/views.py
+++ server/main/api/views.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view, APIView
 from django.http import HttpRequest
 from .models import Movie
+from django.db.models.functions import Lower
+
+
 from django.db.models import Q
 from rest_framework import status
 from django.http import HttpResponse
@@ -21,25 +24,34 @@ class MovieView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
 class SearchMovieView(APIView):
     def post(self, request):
         search_query = request.data.get('query', '')
-        search_results = Movie.objects.filter(
-            Q(name__icontains=search_query) |
-            Q(genre__name__icontains=search_query) |
-            Q(hashtag__name__icontains=search_query) |
-            Q(download_link__icontains=search_query) |
-            Q(description__icontains=search_query) |
-            Q(source__icontains=search_query) |
-            Q(release_date__icontains=search_query) |
-            Q(thumbnail_url__icontains=search_query) |
-            Q(Trailer_url__icontains=search_query) |
-            Q(director__icontains=search_query)
-        )
+        print(f"Received search query: {search_query}")
 
-        serializer = MovieSerializer(search_results, many=True)
-        return Response(serializer.data)
+        if search_query is not None:
+            # Use Lower() to perform case-insensitive search
+            search_results = Movie.objects.filter(
+                Q(name__icontains=search_query) |
+                Q(genre__icontains=search_query) |  # assuming 'genre' is a ForeignKey to another model
+                Q(hashtag__icontains=search_query) |  # assuming 'hashtag' is a ForeignKey to another model
+                Q(download_link__icontains=search_query) |
+                Q(description__icontains=search_query) |
+                Q(source__icontains=search_query) |
+                Q(thumbnail_url__icontains=search_query) |
+                Q(Trailer_url__icontains=search_query) |
+                Q(director__icontains=search_query) |
+                Q(release_date__icontains=search_query)  # This should work for DateField
+            )
 
+            # Use Lower() to perform case-insensitive ordering by name
+            search_results = search_results.order_by(Lower('name'))
+
+            serializer = MovieSerializer(search_results, many=True)
+            return Response(serializer.data)
+        else:
+            return Response("Invalid search query", status=status.HTTP_400_BAD_REQUEST)
 
 
 
